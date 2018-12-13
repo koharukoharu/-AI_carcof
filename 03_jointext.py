a7affa98152d5b6a5a4e747758cf912f5de5ca16
masayoshi-sakino <masayoshi_sakino@waku-2.com>
2018-12-13 18:14:09 +0900

    コメント追加

--- 03_jointext.py
+++ 03_jointext.py
@@ -5,6 +5,7 @@ path = "./study/new_tweet.txt"
 tuika = ""
 
 #追加textをtwitterから取得し、書き込み用データにする。
+#RTやURLの削除リプレース処理が必要
 
 if os.path.isfile(path):
     contents = open(path, "a+", encoding="utf8", errors="ignore")
