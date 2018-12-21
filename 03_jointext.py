336e4b90e71a13b76181c8efa7edd8a1f1433b3c
masayoshi-sakino <masayoshi_sakino@waku-2.com>
2018-12-21 18:45:04 +0900

    twitterAPI add

--- 03_jointext.py
+++ 03_jointext.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import os
 from requests_oauthlib import OAuth1Session
-import config, json
+import config
+import json
 
 path = "./study/new_tweet.txt"
 tuika = ""
@@ -13,13 +14,31 @@ def create_addtext():
     consumer_secret = config.consumer_secret
     access_token = config.access_token
     access_token_seacret = config.access_token_seacret
-    
-    oath = OAuth1Session(consumer_key, consumer_secret, access_token, access_token_seacret)
+
+    twitter = OAuth1Session(consumer_key, consumer_secret, access_token, access_token_seacret)
 
     #twitterAPIからリストのTLを日付フィルタして取得する
     url = "https://api.twitter.com/1.1/lists/statuses.json" #リストのエンドポイント
-    
 
+    #since_IDをファイルで保持させて読み込むと前回取得ツイートの続きから取得できそう
+
+    since_id_read = open("since.txt", "r", encoding="utf8", errors="ignore")
+    since_id_load = since_id_read.read()
+    since_id_read.close()
+
+    list_id = "?list_id=xxx"
+    count = "&count=" + "200"
+    since_id = "&since_id=" + since_id_load
+    url_list = url  + list_id + count + since_id
+
+    req = twitter.get(url_list)
+
+    if req.status_code == 200:
+        timeline = json.loads(req.text)
+        for tweet in timeline:
+            tuika = tuika + tweet['text']
+    else:
+        print("no connection" + url_list)
 
     jointext(tuika)
     
@@ -31,8 +50,7 @@ def jointext(tuika):
     if os.path.isfile(path):
         contents = open(path, "a+", encoding="utf8", errors="ignore")
         contents.write(tuika + "\n")
-    contents.close()
-
+        contents.close()
 
 if __name__ == '__main__':
     create_addtext()
